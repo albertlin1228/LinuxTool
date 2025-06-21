86ab0b93feb953ca218edd0cbdc5c2ac58fbea89
albertlin.ampere <albertlin@amperecomputing.com>
2025-06-21 23:47:48 +0800

    Implement HeaderType, BAR information

--- PciBusEnum.py
+++ PciBusEnum.py
@@ -1,5 +1,5 @@
 import os
-
+import subprocess
 """
 from periphery import MMIO
 
@@ -30,6 +30,21 @@ GlobalExistDevice = 0
 GlobalVendorId = 0
 GlobalDeviceId = 0
 
+# Bit definition
+BIT0 = 1
+BIT1 = 2
+BIT2 = 4
+BIT3 = 8
+BIT4 = 16
+BIT5 = 32
+
+# Pci configuration space offset
+OFFSET_HEADER_TYPE = f"0xE" # Header type offset
+OFFSET_32_BAR0_TYPE = f"0x10" # 32 bit BAR0 offset
+
+OFFSET_64_BAR1_TYPE = f"0x18" # 64 bit BAR1 offset
+OFFSET_64_BAR2_TYPE = f"0x20" # 64 bit BAR2 offset
+
 class Device(object):
 
     __base = "/sys/bus/pci/devices/"
@@ -57,6 +72,67 @@ class Device(object):
             # Print DeviceId
             with open(self.__device,'r') as file:
                 GlobalDeviceId = file.read()
+
+def ListPciRegContent(HexVendor,HexDevice):
+    LsPciCmd = f"sudo lspci -d {str(HexVendor)}:{str(HexDevice)} -xxx"
+    #print("LsPciCmd:",LsPciCmd)
+    RegisterDump = subprocess.run(LsPciCmd, capture_output=True, text=True, shell=True)
+    print(RegisterDump.stdout)
+
+def CheckTypeHeader(HexVendor,HexDevice):
+    SetPciCmd = f"sudo setpci -d {str(HexVendor)}:{str(HexDevice)} {str(OFFSET_HEADER_TYPE)}.b" 
+    #print("SetPciCmd:",SetPciCmd)  
+    RegisterDump = subprocess.run(SetPciCmd, capture_output=True, text=True, shell=True)       
+    #print("HeaderTypeValue:",RegisterDump.stdout)
+    #
+    # Check Type0/Type1, Multifunction
+    #
+    HeaderLayout = int(RegisterDump.stdout) & 0x7F
+    MultiFun = (int(RegisterDump.stdout) & 0x80) >> 7
+    #print("HeaderLayout:",HeaderLayout,"MultiFun:",MultiFun)
+    return HeaderLayout,MultiFun
+
+def CheckBarRegister(HexVendor,HexDevice):
+    Bar32ValueArray = {}
+    Bar64ValueArray = {}
+
+    SetPciCmd = f"sudo setpci -d {str(HexVendor)}:{str(HexDevice)} {str(OFFSET_32_BAR0_TYPE)}.l"
+    RegisterDump = subprocess.run(SetPciCmd, capture_output=True, text=True, shell=True)
+    Bar0Value = int(RegisterDump.stdout,16)
+
+    if (Bar0Value & BIT0) == True:
+        print("IO space indicator")
+    else:
+        if ((Bar0Value & (BIT1 | BIT2)) >> 1) == BIT1:
+            print("64 bits Memory space indicator")
+
+            for BarIndex in range(6): 
+                SetPciCmd = f"sudo setpci -d {str(HexVendor)}:{str(HexDevice)} {str( hex(int(OFFSET_32_BAR0_TYPE,16) + 4*BarIndex))}.l"
+                #print("SetPciCmd:",SetPciCmd)
+                RegisterDump = subprocess.run(SetPciCmd, capture_output=True, text=True, shell=True)
+                BarValue = int(RegisterDump.stdout,16)
+                Bar64ValueArray[BarIndex] = BarValue
+
+            Bar0Value = f"{str(hex(Bar64ValueArray[1]))} {str(hex(Bar64ValueArray[0]))}"
+            print("BAR 0 :",Bar0Value)
+            Bar1Value = f"{str(hex(Bar64ValueArray[3]))} {str(hex(Bar64ValueArray[2]))}"
+            print("BAR 1 :",Bar1Value)
+            Bar2Value = f"{str(hex(Bar64ValueArray[5]))} {str(hex(Bar64ValueArray[4]))}"
+            print("BAR 2 :",Bar2Value)
+
+        elif ((Bar0Value & (BIT1 | BIT2)) >> 1) == 0:
+            print("32 bits Memory space indicator")
+            print("BAR 0 :",hex(Bar0Value))
+            Bar32ValueArray[0] = Bar0Value
+            for BarIndex in range(1,6): 
+                SetPciCmd = f"sudo setpci -d {str(HexVendor)}:{str(HexDevice)} {str( hex(int(OFFSET_32_BAR0_TYPE,16) + 4*BarIndex))}.l"
+                #print("SetPciCmd:",SetPciCmd)
+                RegisterDump = subprocess.run(SetPciCmd, capture_output=True, text=True, shell=True)
+                BarValue = int(RegisterDump.stdout,16)
+                Bar32ValueArray[BarIndex] = BarValue
+                print("BAR",BarIndex,":",hex(Bar32ValueArray[BarIndex]))
+                
+         
 """
     def GetVendor(self, pciid:str):
         self.__vendor = f"{self.__base}/vendor"
--- main.py
+++ main.py
@@ -5,24 +5,41 @@ import array
 import PciBusEnum
 
 # Global variable
-PfaNumberArray = []
-PfaNumberIndex = 0
+gPfaNumberArray = []
+gPfaNumberIndex = 0
+gHexVendor = {}
+gHexDevice = {}
 
 def PciEnumOperation():
-    global PfaNumberArray
-    global PfaNumberIndex
+    global gPfaNumberArray
+    global gPfaNumberIndex
+    global gHexVendor
+    global gHexDevice
 
     for Segment in range(8):
         for Bus in range(256):
             for Dev in range(32):
                 for Fun in range(8):
+                    # PfaNumber = 0007:00:01.0
                     PfaNumber = f"{str(Segment).zfill(4)}:{str(Bus).zfill(2)}:{str(Dev).zfill(2)}.{str(Fun)}"
                     PciBusEnum.Device(PfaNumber)
 
                     if PciBusEnum.GlobalExistDevice == 1:
-                        PfaNumberArray.append(PfaNumber)
-                        print("Index",PfaNumberIndex,":",PfaNumber, " VendorId:",PciBusEnum.GlobalVendorId, "DeviceId:", PciBusEnum.GlobalDeviceId)
-                        PfaNumberIndex += 1
+                        gPfaNumberArray.append(PfaNumber)
+                        
+                        # Save Vid and Did from string file of the /sys/bus/pci/devices/ to hex digit
+                        DigitVendor = int(PciBusEnum.GlobalVendorId,16)
+                        DigitDevice = int(PciBusEnum.GlobalDeviceId,16)
+                        HexVendor = hex(DigitVendor)
+                        HexDevice = hex(DigitDevice)
+                        
+                        gHexVendor[gPfaNumberIndex] = HexVendor
+                        gHexDevice[gPfaNumberIndex] = HexDevice
+
+                        print("Index",gPfaNumberIndex,":",PfaNumber," VendorId:",HexVendor.zfill(4)," DeviceId:",HexDevice.zfill(4) )
+
+                        gPfaNumberIndex += 1
+
 
 # Main prompt y
 print("Select Operation:\n0:Select PCI enum operation\n")
@@ -35,10 +52,22 @@ match Select:
         while True:
             WhichDevice = int(input("Which device?"))
 
-            while (WhichDevice <= PfaNumberIndex):
+            while (WhichDevice <= gPfaNumberIndex):
+                PciBusEnum.ListPciRegContent(gHexVendor[WhichDevice],gHexDevice[WhichDevice])
                 """
-                print("Device index",WhichDevice,":",PfaNumberArray[WhichDevice])
-                SelectDevice = PciBusEnum.Device(PfaNumberArray[WhichDevice])
-                SelectDevice.GetVendor(PfaNumberArray[WhichDevice])
+                print("Device index",WhichDevice,":",gPfaNumberArray[WhichDevice])
+                SelectDevice = PciBusEnum.Device(gPfaNumberArray[WhichDevice])
+                SelectDevice.GetVendor(gPfaNumberArray[WhichDevice])
                 """
+
+                while True:
+                    HeaderLayout,MultiFun = PciBusEnum.CheckTypeHeader(gHexVendor[WhichDevice],gHexDevice[WhichDevice])
+                    if HeaderLayout == 0:
+                        print("Type 0/Pci device")
+                        PciBusEnum.CheckBarRegister(gHexVendor[WhichDevice],gHexDevice[WhichDevice])
+                    else:
+                        print("Type 1/Pci bridge")    
+
+                    break
+
                 break
